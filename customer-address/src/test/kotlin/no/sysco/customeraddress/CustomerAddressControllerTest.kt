fce49efdce6fb5c79052ac572c45fa842fb58762
Personal <personal@Yongs-MBP.home>
2022-05-03 22:26:24 +0200

    add first test for case where payload is valid

--- customer-address/src/test/kotlin/no/sysco/customeraddress/CustomerAddressControllerTest.kt
+++ customer-address/src/test/kotlin/no/sysco/customeraddress/CustomerAddressControllerTest.kt
@@ -1,12 +1,32 @@
 package no.sysco.customeraddress
 
+import no.sysco.customeraddress.dto.CustomerAddressDto
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.postForEntity
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CustomerAddressControllerTest {
+
+    @LocalServerPort
+    private var port: Int = 0
+
+    private val restTemplate = TestRestTemplate()
+
+    private val baseUrl = "http://localhost:$port"
+
+    private val customerAddressUrl = "$baseUrl/customer/address"
+
 
     @Test
     fun `valid payload is correctly deserialized to dto`() {
-
+        val payload = CustomerAddressDto(email = "test@mail.com", physicalAddress = "test street 123C")
+        Assertions.assertEquals(ResponseEntity<HttpStatus>(HttpStatus.OK), restTemplate.postForEntity<HttpStatus>(customerAddressUrl, payload, mapOf("id" to "valid_id")))
     }
 
     @Test
