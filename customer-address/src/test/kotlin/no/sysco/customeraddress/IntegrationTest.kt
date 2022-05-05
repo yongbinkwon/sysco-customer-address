197fbbc096f59b4f14d2e47d9cb272fcebb4deb6
yongbinkwon <yongbinkwon97@gmail.com>
2022-05-05 22:01:14 +0200

    add all CustomerAddressController tests to IntegrationTest

--- customer-address/src/test/kotlin/no/sysco/customeraddress/IntegrationTest.kt
+++ customer-address/src/test/kotlin/no/sysco/customeraddress/IntegrationTest.kt
@@ -4,33 +4,69 @@ import no.sysco.customeraddress.kafka.ScheduledKafkaMessageCache
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @ExtendWith(SpringExtension::class)
 @WebMvcTest(controllers = [CustomerAddressController::class])
 internal class IntegrationTest @Autowired constructor(
-    private val mockMvc: MockMvc
+    private val mockMvc: MockMvc,
+
+    @Value("\${http-ok-message.suspicious-email}")
+    private val suspiciousEmailMsg: String,
+
+    @Value("\${http-ok-message.valid}")
+    private val validMsg: String,
 ) {
 
     @MockBean
     private lateinit var scheduledKafkaMessageCacheMock: ScheduledKafkaMessageCache
 
+    private fun MockMvc.mockPostCall(
+        payloadPath: String,
+        customerId: String? = "valid_id",
+    ) = perform(
+        MockMvcRequestBuilders
+            .post("/customer/address")
+            .queryParam("id", customerId)
+            .content(IntegrationTest::class.java.getResource(payloadPath)?.readText() ?: "")
+            .contentType(MediaType.APPLICATION_JSON)
+    )
+
     @Test
     internal fun `valid payload is correctly deserialized to dto`() {
+        mockMvc.mockPostCall("/ValidPayload.json")
+            .andExpect(status().isOk)
+            .andExpect(content().string(validMsg))
+    }
 
-        mockMvc.perform(
-            MockMvcRequestBuilders
-                .post("/customer/address")
-                .queryParam("id", "valid_id")
-                .content(IntegrationTest::class.java.getResource("/ValidPayload.json")?.readText() ?: "")
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andExpect(status().isOk)
+    @Test
+    internal fun `payload missing any field returns 400 bad request`() {
+        mockMvc.mockPostCall("/InvalidPayloadNoEmail.json")
+            .andExpect(status().isBadRequest)
+
+        mockMvc.mockPostCall("/InvalidPayloadNoPhysicalAddress.json")
+            .andExpect(status().isBadRequest)
+    }
+
+    @Test
+    internal fun `invalid email returns 200 with a warning message`() {
+        mockMvc.mockPostCall("/ValidPayloadTypoEmail.json")
+            .andExpect(status().isOk)
+            .andExpect(content().string(suspiciousEmailMsg))
+    }
+
+    @Test
+    internal fun `no id in query param returns 400 bad request`() {
+        mockMvc.mockPostCall("/ValidPayload.json", null)
+            .andExpect(status().isBadRequest)
 
     }
 }
