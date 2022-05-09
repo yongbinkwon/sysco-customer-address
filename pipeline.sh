5ecc8a88632d30f39e743db6bb64d37209593fab
bin <yongbinkwon97@gmail.com>
2022-05-09 14:43:27 +0200

    remove gradle build from pipeline script

--- pipeline.sh
+++ pipeline.sh
@@ -1,4 +1,4 @@
-./gradlew clean build #probably isn't needed?
+#./gradlew clean build not sure if needed
 docker-compose down
 docker-compose up -d
 docker-compose exec broker kafka-topics --create --bootstrap-server broker:9092 --replication-factor 1 --partitions 1 --topic sysco-customer-address-v1
